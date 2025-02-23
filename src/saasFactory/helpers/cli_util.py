cbf70e1515b0eeef00aca9da07f1f2a138402c05
swilliams <smwilliams@mines.edu>
2025-02-23 16:19:54 -0700

    added docs to cli utils and added type safety

--- src/saasFactory/helpers/cli_util.py
+++ src/saasFactory/helpers/cli_util.py
@@ -1,7 +1,18 @@
 import os
-import datetime
+from typing import Any, Dict, List, Tuple
+from datetime import datetime
 
-def createProjectDir(projectName):
+
+def createProjectDir(projectName: str) -> str|None:
+    """
+    Creates a project directory with the given project name.
+
+    Args:
+        projectName (str): The name of the project.
+
+    Returns:
+        str: The absolute path of the created project directory, or None if an error occurred.
+    """
     try:
         project_path = os.path.abspath(projectName)
         if not os.path.exists(project_path):
@@ -13,7 +24,16 @@ def createProjectDir(projectName):
         return None
 
 
-def createEnvFile(project_path):
+def createEnvFile(project_path: str) -> None:
+    """
+    Creates a .env file in the specified project directory.
+
+    Args:
+        project_path (str): The path to the project directory.
+
+    Returns:
+        None
+    """
     try:
         if os.path.exists(os.path.join(project_path, ".env")):
             print("Project already initialized.")
@@ -26,7 +46,17 @@ def createEnvFile(project_path):
         print(f"Error creating .env file: {e}")
 
 
-def createSFConfigFile(project_path, project_name):
+def createSFConfigFile(project_path: str, project_name: str) -> None:
+    """
+    Creates an sf_config.yaml file in the specified project directory.
+
+    Args:
+        project_path (str): The path to the project directory.
+        project_name (str): The name of the project.
+
+    Returns:
+        None
+    """
     config_file_name = "sf_config.yaml"
     try:
         if os.path.exists(os.path.join(project_path, config_file_name)):
@@ -42,7 +72,13 @@ def createSFConfigFile(project_path, project_name):
         print(f"Error creating sf_config.yaml file: {e}")
 
 
-def findProjectRoot():
+def findProjectRoot() -> str|None:
+    """
+    Finds the root directory of the project by looking for the sf_config.yaml file.
+
+    Returns:
+        str: The path to the sf_config.yaml file if found, otherwise None.
+    """
     current_dir = os.getcwd()
     config_path = os.path.join(current_dir, "sf_config.yaml")
     if os.path.exists(config_path):
